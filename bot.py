43b34ebefd639506e158003ffa8331cfa9ccbd38
aybekko97 <aybekko97@gmail.com>
2017-08-21 10:24:47 +0600

    [Added] - included aiohttp 2.

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import datetime
 import telebot
 from telebot.types import LabeledPrice
 
+from aiohttp import web
+
 from Main import HousePricing
 from flat import Flat
 from validations import *
@@ -46,6 +48,20 @@ print("Finished training!")
 prices = [LabeledPrice(label='House Agent Service', amount=5750), LabeledPrice('Gift wrapping', 500)]
 
 bot = telebot.AsyncTeleBot(token, threaded=True)
+app = web.Application()
+
+# Process webhook calls
+async def handle(request):
+    if request.match_info.get('token') == bot.token:
+        request_body_dict = await request.json()
+        update = telebot.types.Update.de_json(request_body_dict)
+        bot.process_new_updates([update])
+        return web.Response()
+    else:
+        return web.Response(status=403)
+
+app.router.add_post('/{token}/', handle)
+
 
 flat_dict = {}
 step = {}
@@ -346,7 +362,23 @@ def callback_inline(call):
                                           reply_markup=keyboard)
 
 
+bot.skip_pending = True
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
 bot.remove_webhook()
 
-bot.skip_pending = True
-bot.polling(none_stop=True)
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# Build ssl context
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+
+# Start aiohttp server
+web.run_app(
+    app,
+    host=WEBHOOK_LISTEN,
+    port=WEBHOOK_PORT,
+    ssl_context=context,
+)
